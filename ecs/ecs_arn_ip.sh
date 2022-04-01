743a345a2964b18232ec52402d06eb94b1e7ac0f
Jhonny Munerati <jhmt84@yahoo.com.br>
2022-04-01 00:31:57 -0300

    Corrigindo ecs_arn_ip.sh

--- ecs/ecs_arn_ip.sh
+++ ecs/ecs_arn_ip.sh
@@ -15,7 +15,7 @@ aws ecs list-tasks --cluster $cluster --service-name $service | grep arn | cut -
 
 while IFS= read -r linha; do
 	aws ecs describe-tasks --cluster $cluster --tasks $linha | grep -e \"taskArn\": -e "\"privateIpv4Address\":\ \"$privateIpv4Addres\"" -m 2 > ecs_arn_ip_temp.txt
-	search=$(grep -m 1 privateIpv4Address ecs_arn_ip_temp.txt | cut -c 52-63)
+	search=$(grep -o -m 1 $privateIpv4Addres ecs_arn_ip_temp.txt)
 	if [[ "$privateIpv4Addres" == "$search" ]]; then
 	cp ecs_arn_ip_temp.txt ecs_arn_ip_encontrada.txt
 	echo "Encontrada!!!"
