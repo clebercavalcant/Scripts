5cb3d931234a6e31ed8cf8c24fd9eaac5be1e388
Jhonny <jhmt84@yahoo.com.br>
2022-04-04 09:02:14 -0300

    Corrigindo acesso_AWS

--- acesso_AWS.sh
+++ acesso_AWS.sh
@@ -11,8 +11,6 @@ repetir=y
 while [ $repetir = y ]; do
 echo "O profile padrão é o default. Os scripts estão configurados para o mesmo no momento. Digite "default" ou outro nome específico:"
 read profile
-echo "[$profile]" >> credentials
-echo "[$profile]" >> config
 echo "Digite a region default:"
 read region
 echo "Digite a AWS Access Key Id:"
@@ -21,10 +19,23 @@ echo "Digite a AWS Secret access key:"
 read aws_secret_access_key
 echo "Digite a AWS session token:"
 read aws_session_token
+echo "Confira os dados: 
+region: $region
+aws_access_key_id: $aws_access_key_id
+aws_secret_access_key: $aws_secret_access_key
+aws_session_token: $aws_session_token"
+echo "Estão corretos? Se sim digite \(y\), se não digite \(n\)."
+read confirm2
+if [ $confirm2 = y ]; then
+echo "[$profile]" >> credentials
+echo "[$profile]" >> config
 echo "region = $region" >> config
 echo "aws_access_key_id = $aws_access_key_id" >> credentials 
 echo "aws_secret_access_key = $aws_secret_access_key" >> credentials
 echo "aws_session_token = $aws_session_token" >> credentials
+else
+echo "Se não digitou correto, tente novamente."
+fi
 echo "Deseja configurar mais perfis? Se sim digite \(y\), se não digite \(n\)."
 read repetir
 done
