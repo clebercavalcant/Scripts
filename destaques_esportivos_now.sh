23844efb916be7e3106a697ecd546921aaaba629
Cleber Cavalcanti <cleberscavalcanti@hotmail.com>
2022-05-05 11:55:27 -0300

    Atualização da condição canais globo

--- destaques_esportivos_now.sh
+++ destaques_esportivos_now.sh
@@ -10,14 +10,15 @@ echo "
 "
 sleep 2
 
-echo "Realizando download do arquivo settings.json!"
-aws s3 cp s3://nowonlinecloud-static/now-be/config/settings.json settings_bkp.json --region sa-east-1
+echo -e "Realizando download do arquivo settings.json!\n"
+aws s3 cp s3://nowonlinecloud-static/now-be/config/settings.json settings_bkp.json --region sa-east-1 > /dev/null 2>&1
 
 	
 echo "Digite os Id's dos canais que irão ser apresentados no carrossel de destaques:"
 echo "Canal id 1:"
 read id1
-	
+##while ['$id1' != '^[0-9$]+$']; do
+ 
 echo "Canal id 2:"
 read id2
 
@@ -33,11 +34,23 @@ read id5
 echo "Canal id 6:"
 read id6
 
-echo "Digite o id do filme que irá ser apresentado no Autoplay:" 
-read id7
+echo "Precisará alterar o filme do Autoplay?"
+echo "Digite y ou n:"
+read decisao
+
+if [ $decisao = "y" ]; then
+
+	echo "Digite o id do filme que irá ser apresentado no Autoplay:" 
+	read id7
 	
-jq '.banners.live.home = ['$id1', '$id2', '$id3', '$id4', '$id5', '$id6']' settings_bkp.json > settings_bkp1.json
-jq '.banners.vodTrailer = ['$id7']' settings_bkp1.json	> settings.json
+	jq '.banners.live.home = ['$id1', '$id2', '$id3', '$id4', '$id5', '$id6']' settings_bkp.json > settings.json
+
+else
+
+	jq '.banners.live.home = ['$id1', '$id2', '$id3', '$id4', '$id5', '$id6']' settings_bkp.json > settings_bkp1.json
+	jq '.banners.vodTrailer = ['$id7']' settings_bkp1.json  > settings.json
+
+fi
 
 echo "Realizando upload do arquivo settings.json!"
 #aws s3 cp settings.json s3://nowonlinecloud-static/now-be/config/settings.json
